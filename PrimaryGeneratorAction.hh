0e10c8c785d39177329f0832bfea5e237d18b4db
Archana-naik0019 <archana.naik@students.iiserpune.ac.in>
2025-05-13 23:56:39 +0530

    Create PrimaryGeneratorAction.hh

---
+++ PrimaryGeneratorAction.hh
@@ -0,0 +1,23 @@
+#ifndef PRIMARY_GENERATOR_ACTION_HH
+#define PRIMARY_GENERATOR_ACTION_HH
+
+#include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ParticleGun.hh"
+
+#include "G4GeneralParticleSource.hh"
+
+class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction {
+public:
+    PrimaryGeneratorAction();
+    virtual ~PrimaryGeneratorAction();
+    virtual void GeneratePrimaries(G4Event* anEvent);
+
+private:
+    G4ParticleGun* fParticleGun;
+    
+    G4GeneralParticleSource* fGPS;
+    G4bool useGPS; // switching between gun and GPS
+    
+};
+
+#endif
